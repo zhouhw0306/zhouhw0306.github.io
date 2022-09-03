30b866fcb3664144240632471fc91194edd90cbc
zhouhw0306 <2466864587@qq.com>
2022-09-03 11:41:10 +0800

    Site updated: 2022-09-03 11:41:06

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "32px 30px 30px 30px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "center",
+      "horizontal": "center",
+      "key": "344a216b67204bae99d09c5508c227d3"
+    }
+  }
